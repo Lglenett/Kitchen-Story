2954e62244684ef349fd6710a43a346eeb5a44fe
Lesley Molepo <lglenett@gmail.com>
2021-07-16 23:46:32 +0200

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,59 @@
+const products = [{
+        name: "Watermelon",
+        imageUrl: "https://images.unsplash.com/photo-1587049352846-4a222e784d38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+        description: "Large juicy organic watermelon straight from our garden.",
+        price: 49,
+        countInStock: 15,
+    },
+    {
+        name: "Bananas",
+        imageUrl: "https://images.unsplash.com/photo-1566393028639-d108a42c46a7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=727&q=80",
+        description: "Tropical organic Bananas",
+        price: 28,
+        countInStock: 10,
+    },
+    {
+        name: "Pomegranate",
+        imageUrl: "https://images.unsplash.com/photo-1608869857121-c0bcdf4fff2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+        description: "one large red pomegranate and one quarter pomegranate on wooden table.",
+        price: 28,
+        countInStock: 20,
+    },
+    {
+        name: "Almond Milk",
+        imageUrl: "https://images.unsplash.com/photo-1563636619-e9143da7973b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+        description: "At first I thought this was the usual milk that I like but it’s actually fermented milk and man it’s really sour! It’s like drinking yoghurt without the sugar",
+        price: 50,
+        countInStock: 25,
+    },
+    {
+        name: "Stake",
+        imageUrl: "https://images.unsplash.com/photo-1448907503123-67254d59ca4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80",
+        description: "Taxa Steak and Herbs.",
+        price: 289,
+        countInStock: 12,
+    },
+    {
+        name: "Onions",
+        imageUrl: "https://images.unsplash.com/photo-1491524062933-cb0289261700?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+        description: "Local farm gave us a box of eggs. I loved capturing the beauty of the different shades and colors of each egg.",
+        price: 140,
+        countInStock: 10,
+    },
+    {
+        name: "Coffee",
+        imageUrl: "https://images.unsplash.com/photo-1524350876685-274059332603?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80",
+        description: "Italian coffee blend with strong aromatics",
+        price: 100,
+        countInStock: 5,
+    },
+    {
+        name: "Oranges",
+        imageUrl: "https://images.unsplash.com/photo-1607364045607-95d87aec4706?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=594&q=80",
+        description: "Fresh organic oranges.",
+        price: 60,
+        countInStock: 22,
+    },
+];
+
+module.exports = products;
